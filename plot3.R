0b145053173ebea051057b7537319780cadcc33b
Gustavo Niño <gustavo.a.nino@gmail.com>
2021-09-24 21:31:32 -0500

    My Solution to week 1 project on exploratory data analysis.
    plot1-4.png
    plot 1-4.R
    run_plotting.R (with all code)

---
+++ plot3.R
@@ -0,0 +1,30 @@
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+if(!file.exists("exdata_data_household_power_consumption")){download.file(url, "exdata_data_household_power_consumption")}
+if(!file.exists("household_power_consumption.txt")){unzip("exdata_data_household_power_consumption.zip")}
+epc<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+## Libraries
+library(lubridate)
+library(dplyr)
+
+
+## change to dates
+
+epc<-mutate(epc, datetime = paste(epc$Date, epc$Time, sep = " ") )
+colss<-c(3:8)
+epc[colss] <-sapply(epc[colss], as.numeric)
+epc$Date <-dmy(epc$Date)
+epc$Time <- hms(epc$Time)
+epc$datetime<-dmy_hms(epc$datetime)
+str(epc)
+
+##subsetting data by date
+x<-subset(epc, epc$Date>=ymd(20070201)&epc$Date<= ymd(20070202))
+
+## plot 3
+plot(x$datetime, x$Sub_metering_1, type="l", xlab="", ylab="Global Active Power (killowatts)")
+lines(x$datetime,x$Sub_metering_2, col="red")
+lines(x$datetime,x$Sub_metering_3, col="blue" )
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), lwd=2)
+dev.copy(png, file="plot3.png", width = 600, height= 600)
+dev.off()
---
+++ run_plotting.R
@@ -0,0 +1,54 @@
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+if(!file.exists("exdata_data_household_power_consumption")){download.file(url, "exdata_data_household_power_consumption")}
+if(!file.exists("household_power_consumption.txt")){unzip("exdata_data_household_power_consumption.zip")}
+epc<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+## Libraries
+library(lubridate)
+library(dplyr)
+
+
+## change to dates
+
+epc<-mutate(epc, datetime = paste(epc$Date, epc$Time, sep = " ") )
+colss<-c(3:8)
+epc[colss] <-sapply(epc[colss], as.numeric)
+epc$Date <-dmy(epc$Date)
+epc$Time <- hms(epc$Time)
+epc$datetime<-dmy_hms(epc$datetime)
+str(epc)
+
+##subsetting data by date
+x<-subset(epc, epc$Date>=ymd(20070201)&epc$Date<= ymd(20070202))
+
+## plot 1
+hist(x$Global_active_power, col="red", main="Global Active Power", xlab="Clobal Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width = 600, height= 600)
+dev.off()
+
+## plot 2
+plot(x$datetime, x$Global_active_power, type="l", xlab="", ylab="Global Active Power (killowatts)")
+dev.copy(png, file="plot2.png", width = 600, height= 600)
+dev.off()
+
+## plot 3
+plot(x$datetime, x$Sub_metering_1, type="l", xlab="", ylab="Global Active Power (killowatts)")
+lines(x$datetime,x$Sub_metering_2, col="red")
+lines(x$datetime,x$Sub_metering_3, col="blue" )
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), lwd=2)
+dev.copy(png, file="plot3.png", width = 600, height= 600)
+dev.off()
+
+## plot 4
+
+par( mfrow=c(2, 2))
+
+plot(x$datetime, x$Global_active_power, type="l", xlab="", ylab="Global Active Power (killowatts)")
+plot(x$datetime, x$Voltage, xlab="datetime", ylab="Voltage", type="l")
+plot(x$datetime, x$Sub_metering_1, type="l", xlab="", ylab="Global Active Power (killowatts)")
+lines(x$datetime,x$Sub_metering_2, col="red")
+lines(x$datetime,x$Sub_metering_3, col="blue" )
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), lwd=2)
+plot(x$datetime, x$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")
+dev.copy(png, file="plot4.png", width = 600, height= 600)
+dev.off()
